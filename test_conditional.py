65811da86da21ae27c855e06b76b9803ab57aba9
Hazik Khawaja <hazik@localhost.localdomain>
2019-11-30 08:56:37 -0500

    added new file to test the if else

---
+++ test_conditional.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.8
+
+# this module is to test python IF..ELIF conditionals
+
+# variables
+choice = 2
+
+#check choices for calculator options
+if choice == 1:
+    print ("addition")
+elif choice == 2:
+    print ("subtraction")
+elif choice == 3:
+    print ("multiplication")
+else:
+    print ("division")
