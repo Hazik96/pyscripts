3f2ca7689d60a009ec29a733051436d9cba4c636
Hazik Khawaja <hazik@localhost.localdomain>
2019-12-07 12:27:27 -0500

    creating and displayin messages using python

---
+++ creating-and-displaying-variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.8
+
+#creating and displaying variables
+first_name= "Muhammad"
+last_name= "Hazik Khawaja"
+age=24
+birth_date="11/27/1996"
+
+print (f"my name is {first_name} {last_name}")
+print (f"I was born on {birth_date}, and i'm {age} years old.")
