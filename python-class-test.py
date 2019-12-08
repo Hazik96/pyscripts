9360f61b3156506a8a41f4e221fead1083ac4d65
Hazik Khawaja <hazik@localhost.localdomain>
2019-12-08 13:17:28 -0500

    print statement

---
+++ python-class-test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+
+# prints the string "Hello world"
+print ("Hello world")
+################################################################
+
+# assigning variables and orinting their value
+name = "John"
+print (name)
+print (f"{name} is nice")
+################################################################
+
